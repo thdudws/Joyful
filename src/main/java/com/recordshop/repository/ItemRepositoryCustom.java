b4904f90873dd76132dc8b239617e17fdacd78ce
seoyoungkyung <dnsdka202@naver.com>
2024-12-30 15:37:58 +0900

    Update ItemRepositoryCustom.java

--- src/main/java/com/recordshop/repository/ItemRepositoryCustom.java
+++ src/main/java/com/recordshop/repository/ItemRepositoryCustom.java
@@ -1,10 +1,13 @@
 package com.recordshop.repository;
 
+import com.recordshop.constant.Category;
 import com.recordshop.dto.ItemSearchDto;
 import com.recordshop.dto.MainItemDto;
 import com.recordshop.entity.Item;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+
+import java.util.List;
 
 public interface ItemRepositoryCustom {
 
@@ -12,5 +15,6 @@ public interface ItemRepositoryCustom {
 
     Page<MainItemDto> getMainItemPage(ItemSearchDto itemSearchDto, Pageable pageable);
 
-    //    List<MainItemDto> findItemsByCategory(Category category);
+//    List<MainItemDto> findItemsByCategory(Category category);
+
 }
