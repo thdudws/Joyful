0a6f55ccb3012925af50eea2671e8b3894b95f81
seoyoungkyung <dnsdka202@naver.com>
2024-12-30 15:30:24 +0900

    Update ItemImgDto.java

--- src/main/java/com/recordshop/dto/ItemImgDto.java
+++ src/main/java/com/recordshop/dto/ItemImgDto.java
@@ -22,6 +22,7 @@ public class ItemImgDto {
     private static ModelMapper modelMapper = new ModelMapper();
 
     public static ItemImgDto of(ItemImg itemImg) {
+
         return modelMapper.map(itemImg, ItemImgDto.class);
     }
 }
