590bb6d427cd9d4a1edcbfadbc5dd46c8b466570
seoyoungkyung <dnsdka202@naver.com>
2025-01-10 17:32:46 +0900

    Update CartRepository.java

--- src/main/java/com/recordshop/repository/CartRepository.java
+++ src/main/java/com/recordshop/repository/CartRepository.java
@@ -6,4 +6,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface CartRepository extends JpaRepository<Cart, String> {
 
     Cart findByMemberName(String username);
+
+    Cart findByMemberId(Long memberId);
+
+    Collection<Object> findByIdIn(Collection<Long> ids);
 }
