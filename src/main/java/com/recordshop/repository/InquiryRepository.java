02e19889ed6e656273bad18b9d9db3330547f5e5
seoyoungkyung <dnsdka202@naver.com>
2025-01-10 17:33:13 +0900

    Update InquiryRepository.java

--- src/main/java/com/recordshop/repository/InquiryRepository.java
+++ src/main/java/com/recordshop/repository/InquiryRepository.java
@@ -9,12 +9,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 import java.util.Optional;
 
-public interface InquiryRepository extends JpaRepository<Inquiry, Long> {
+public interface InquiryRepository extends JpaRepository<Inquiry, String> {
 
     // 회원 이메일로 해당 회원이 작성한 문의글 조회
-    Page<Inquiry> findByMemberEmail(String email, Pageable pageable);
+    Page<Inquiry> findByMemberUsername(String username, Pageable pageable);
 
-    Page<Inquiry> findByMemberEmailAndAnswerStatus(String email, AnswerStatus answerStatus, Pageable pageable);
+    Page<Inquiry> findByMemberUsernameAndAnswerStatus(String username, AnswerStatus answerStatus, Pageable pageable);
 
     // 관리자용: 모든 문의글 조회
     Page<Inquiry> findAll(Pageable pageable);
@@ -25,7 +25,7 @@ public interface InquiryRepository extends JpaRepository<Inquiry, Long> {
 
     Inquiry findByIdAndMemberEmail(Long id, String email);
 
-    List<Inquiry> findByMemberEmail(String email);
+    List<Inquiry> findByMemberUsername(String username);
 
     Optional<Inquiry> findById(Long inqquiryId);
 
